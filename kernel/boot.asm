1134f9bcb02c9b1e81697af2787207c77edc4ab3
Nehal Vala <nikhilvala0005@gmail.com>
2026-09-20 12:17:05 +0000

    Add GDT for kernel and user segments

--- kernel/boot.asm
+++ kernel/boot.asm
@@ -6,20 +6,49 @@ align 4
 
 section .bss
 align 16
+
 stack_bottom:
     resb 16384
 stack_top:
 
 section .text
+
 global start
 extern kernel_main
 
 start:
     cli
+
+    lgdt [gdt_descriptor]
+
+    mov ax, 0x08
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
     mov esp, stack_top
+
+    jmp 0x10:protected_mode
+
+protected_mode:
     call kernel_main
 
 hang:
     cli
     hlt
     jmp hang
+
+section .data
+
+gdt:
+    dq 0x0000000000000000
+    dq 0x00CF92000000FFFF
+    dq 0x00CF9A000000FFFF
+    dq 0x00CFFA000000FFFF
+    dq 0x00CFF2000000FFFF
+
+gdt_descriptor:
+    dw gdt_descriptor - gdt - 1
+    dd gdt
