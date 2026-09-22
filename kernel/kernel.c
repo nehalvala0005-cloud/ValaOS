94c8ab7d7c23de2e3ab9a2b3bf3c948bde7887a0
Nehal Vala <nikhilvala0005@gmail.com>
2026-09-22 07:45:37 +0000

    Restore stable user mode execution

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -11,13 +11,13 @@ extern void task_context_init();
 extern void tss_init();
 
 extern int map_page_flags(
-    unsigned int *virtual_address,
-    unsigned int *physical_address,
+    unsigned int* virtual_address,
+    unsigned int* physical_address,
     unsigned int flags
 );
 
 extern void enter_user_mode();
-
+extern void enter_user_program(unsigned int address);
 extern void user_task_start();
 
 #define PAGE_PRESENT 0x001
@@ -27,10 +27,14 @@ extern void user_task_start();
 static unsigned char user_code_page[4096]
     __attribute__((aligned(4096)));
 
+static unsigned char user_test_page[4096]
+    __attribute__((aligned(4096)));
+
 static unsigned char user_stack_page[4096]
     __attribute__((aligned(4096)));
 
 void user_mode_init() {
+
     user_code_page[0] = 0xB8;
     user_code_page[1] = 0x01;
     user_code_page[2] = 0x00;
@@ -61,9 +65,47 @@ void user_mode_init() {
     user_code_page[21] = 0xEB;
     user_code_page[22] = 0xFE;
 
+
+    user_test_page[0] = 0xB8;
+    user_test_page[1] = 0x01;
+    user_test_page[2] = 0x00;
+    user_test_page[3] = 0x00;
+    user_test_page[4] = 0x00;
+
+    user_test_page[5] = 0xBB;
+    user_test_page[6] = 'T';
+    user_test_page[7] = 0x00;
+    user_test_page[8] = 0x00;
+    user_test_page[9] = 0x00;
+
+    user_test_page[10] = 0xCD;
+    user_test_page[11] = 0x80;
+
+    user_test_page[12] = 0xB8;
+    user_test_page[13] = 0x02;
+    user_test_page[14] = 0x00;
+    user_test_page[15] = 0x00;
+    user_test_page[16] = 0x00;
+
+    user_test_page[17] = 0x31;
+    user_test_page[18] = 0xDB;
+
+    user_test_page[19] = 0xCD;
+    user_test_page[20] = 0x80;
+
+    user_test_page[21] = 0xEB;
+    user_test_page[22] = 0xFE;
+
+
     map_page_flags(
         (unsigned int*)0x00400000,
         (unsigned int*)user_code_page,
+        PAGE_PRESENT | PAGE_USER
+    );
+
+    map_page_flags(
+        (unsigned int*)0x00500000,
+        (unsigned int*)user_test_page,
         PAGE_PRESENT | PAGE_USER
     );
 
@@ -75,6 +117,7 @@ void user_mode_init() {
 }
 
 void kernel_main() {
+
     __asm__ volatile ("cli");
 
     clear_screen();
@@ -91,6 +134,7 @@ void kernel_main() {
     print("========================================\n\n");
 
     print("ValaOS Kernel v1.0\n\n");
+
     print("[ OK ] Kernel loaded\n");
     print("[ OK ] VGA initialized\n");
     print("[ OK ] Keyboard driver initialized\n");
@@ -98,13 +142,13 @@ void kernel_main() {
 
     print("Welcome to ValaOS!\n\n");
 
-   print("ValaOS> ");
+    print("ValaOS> ");
 
-keyboard_init();
+    keyboard_init();
 
-__asm__ volatile ("sti");
+    __asm__ volatile ("sti");
 
-while (1) {
-    keyboard_poll();
-}
+    while (1) {
+        keyboard_poll();
+    }
 }
